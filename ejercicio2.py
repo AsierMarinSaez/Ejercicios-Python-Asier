e7419d3e87f9fc4b35a92fd597f1dba95bf47ef9
Asier Marin <136708387+AsierMarinSaez@users.noreply.github.com>
2025-03-31 19:39:51 +0200

    Update ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,8 +1,15 @@
+__Author__="Asier Marin Saez"
+
 """Pide la edad y el nombre por teclado. En caso de ser mayor de 18 años indica que ya puede conducir."""
 
 def main():
-    nombre=input("Introduzca su nombre: ");
+    nombre=input("Introduzca su nombre: ")
     edad=int(input(f"Introduzca su edad {nombre}: "))
+
+    if edad >= 18:
+        print(f"{nombre} ya puede conducir")
+    else:
+        print(f"{nombre} todavía eres menor de edad")
 
     # Comprobamos si es mayor de edad - Estructura condicional if - else
     # Si edad mayor o igual a dieciocho --> Usted es nayor de edad
